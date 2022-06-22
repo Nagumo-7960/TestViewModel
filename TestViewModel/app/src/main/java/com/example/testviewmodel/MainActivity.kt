0a4528e4923c51a6912d0380ff8fea319eee0dd4
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-22 13:32:01 +0900

    画面の中央にボタンを配置

--- TestViewModel/app/src/main/java/com/example/testviewmodel/MainActivity.kt
+++ TestViewModel/app/src/main/java/com/example/testviewmodel/MainActivity.kt
@@ -3,36 +3,36 @@ package com.example.testviewmodel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.testviewmodel.ui.theme.TestViewModelTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TestViewModelTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                }
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                NonFavoriteButton()
             }
         }
     }
 }
+
 
 @Composable
 fun NonFavoriteButton() {
@@ -62,7 +62,8 @@ fun NonFavoriteButton() {
         Text(
             "お気に入り",
             fontWeight = FontWeight.Bold
-        )    }
+        )
+    }
 }
 
 @Composable
